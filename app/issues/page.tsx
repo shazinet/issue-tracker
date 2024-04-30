02c7ec5c5ff5664144e458920cdcde202a2250f8
Hoomaan <id@hoomaan.dev>
2024-04-30 07:23:07 +0400

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from "@/prisma/client";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import { Table } from "@radix-ui/themes";
 import React from "react";
+import Link from "../components/Link";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
 import IssueToolbar from "./IssueToolbar";
