927529a402ead07d7908f80bfecdeb6f2c06cd95
Hoomaan <id@hoomaan.dev>
2024-05-01 22:09:52 +0400

    Improve the loading experience

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,12 @@
-import IssueForm from "../_components/IssueForm";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
-const NewIssuePage = () => {
+const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
+
+const NewIssuePage = async () => {
   return <IssueForm />;
 };
 
