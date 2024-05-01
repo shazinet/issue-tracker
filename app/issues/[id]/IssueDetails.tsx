17d50e35c2117138ec0028b64d8c835b2139f8ce
Hoomaan <id@hoomaan.dev>
2024-05-01 16:29:32 +0400

    Refactor: Apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,23 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import Markdown from "react-markdown";
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+  return (
+    <>
+      <Heading>
+        {issue.id}: {issue.title}
+      </Heading>
+      <Flex gap="3" align="center" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,41 +1,27 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Box, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import Markdown from "react-markdown";
-import { Pencil2Icon } from "@radix-ui/react-icons";
-import Link from "next/link";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueSinglePage = async ({ params }: Props) => {
-  const isssue = await prisma.issue.findUnique({
+  const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
-  if (!isssue) notFound();
+  if (!issue) notFound();
 
   return (
     <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-        <Heading>
-          {isssue.id}: {isssue.title}
-        </Heading>
-        <Flex gap="3" align="center" my="2">
-          <IssueStatusBadge status={isssue.status} />
-          <Text>{isssue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className="prose" mt="4">
-          <Markdown>{isssue.description}</Markdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${isssue.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
