62dcd29202d21d2b34f028785f79bb20c369df72
Hoomaan <id@hoomaan.dev>
2024-05-03 21:34:17 +0400

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -11,24 +11,29 @@ interface Props {
 }
 
 const IssuePage = async ({ searchParams }: Props) => {
-  console.log(searchParams.status);
+  const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issues", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
 
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
-
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issues", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ];
 
   return (
     <div>
