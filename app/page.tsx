fc17edf443dfd425f3e579a3fd5665c1898613d3
Hoomaan <id@hoomaan.dev>
2024-05-04 08:57:34 +0400

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
-import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
       <div>Hello World!</div>
-      <Pagination itemCount={100} currentPage={1} pageSize={10} />
+      <Pagination
+        itemCount={100}
+        currentPage={parseInt(searchParams.page) || 1}
+        pageSize={10}
+      />
     </>
   );
 }
