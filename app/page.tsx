001dbdabc23ad017558b452c5c3d2c698031e13d
Hoomaan <id@hoomaan.dev>
2024-05-04 21:28:05 +0400

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
     <>
-      <div>Hello World!</div>
+      <LatestIssues />
     </>
   );
 }
