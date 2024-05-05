f4b6117fe3b782ea2e74e4604d7a1883254bd515
Hoomaan <id@hoomaan.dev>
2024-05-05 18:44:18 +0400

    Change relationMode to prisma

--- prisma/migrations/20240505144157_initial_migration/migration.sql
+++ prisma/migrations/20240505144157_initial_migration/migration.sql
@@ -1,29 +1,15 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE `Issue` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'CLOSED') NOT NULL DEFAULT 'OPEN',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `assignedToUserId` VARCHAR(255) NULL,
 
-  - You are about to drop the `Account` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `VerificationRequest` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropForeignKey
-ALTER TABLE `Account` DROP FOREIGN KEY `Account_userId_fkey`;
-
--- DropForeignKey
-ALTER TABLE `Session` DROP FOREIGN KEY `Session_userId_fkey`;
-
--- DropTable
-DROP TABLE `Account`;
-
--- DropTable
-DROP TABLE `Session`;
-
--- DropTable
-DROP TABLE `User`;
-
--- DropTable
-DROP TABLE `VerificationRequest`;
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `accounts` (
@@ -75,9 +61,3 @@ CREATE TABLE `verificationtokens` (
 
     UNIQUE INDEX `verificationtokens_identifier_token_key`(`identifier`, `token`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `accounts` ADD CONSTRAINT `accounts_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
